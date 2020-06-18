f3002db0abb04df21bd4ea08a7571b252b77ada7
Hicham <hichoh00@gmail.com>
2020-06-18 14:22:01 -0400

    add files

---
+++ main.js
@@ -0,0 +1,6 @@
+const parentelement = document.querySelector('.katas-list')
+//kata 1
+const kata1heading = document.createElement('h3')
+
+kata1heading.append('kata 1')
+parentelement.append(kata1heading)
